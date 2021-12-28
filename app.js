d67ab1b7ae0102788f4f8b8e10eb684d689ddd66
Tushar <you@example.com>
2021-12-28 14:06:11 +0530

    Final Commit

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 //jshint esversion:6
 
 const express = require("express");
+const _ = require("lodash");
 const bodyParser = require("body-parser");
 const app = express();
 const mongoose = require('mongoose');
@@ -8,11 +9,34 @@ const User = require('./model/user');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const JWT_SECRET = 'sdjkfh8923yhjdksbfma@#*(&@*!^#&@bhjb2qiuhesdbhjdsfg839ujkdhfjk'
+const request = require("request");
+const fetch = require("node-fetch");
+var rating=0;
+
+const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
 
 mongoose.connect("mongodb://localhost:27017/loginDB", {
   useNewUrlParser: true,
   useUnifiedTopology: true
+});
+
+const composeSchema = ({
+  title: String,
+  body: String,
+  link: String
+});
+const Blog = new mongoose.model("Blog", composeSchema);
+const homeStartingContent = new Blog({
+  title: "Home",
+  body: "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.",
+  link: "home"
+});
+Blog.find({}, function(err, found) {
+  if (found.length === 0) {
+    homeStartingContent.save();
+  }
+  // console.log("Success");
 });
 
 app.set('view engine', 'ejs');
@@ -27,6 +51,50 @@ app.get("/", function(req, res) {
 });
 app.get("/signup", function(req, res) {
   res.render("signup");
+});
+
+app.post("/search", function(req, res) {
+  hotelName = req.body.hotelName;
+  Blog.find({}, function(err, found) {
+    res.render("review", {
+      content: found,
+      hotelName: hotelName
+    });
+  });
+});
+app.get("/compose", function(req, res) {
+  res.render("compose",{rating:0});
+});
+
+app.get("/contact",function(req,res){
+  res.render("contact",{contactContent:contactContent});
+});
+
+app.post("/compose", async(req, res)=> {
+  const post = new Blog({
+    title: req.body.postTitle,
+    body: req.body.postBody,
+    link: _.lowerCase(req.body.postTitle)
+  });
+  post.save();
+  const data = {
+    text: req.body.postBody
+  };
+  const response = await fetch("https://hotel-rating.herokuapp.com/rating", {
+    method: 'post',
+    body: JSON.stringify(data),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  });
+  const data1 = await response.json();
+  // console.log(data1);
+  rating=JSON.parse(data1).result[0];
+  // console.log(typeof rating);
+  // console.log(data1.result[0]);
+  // console.log(rating);
+  // res.render("compose",{rating:rating});
+  res.render("rating",{rating:rating});
 });
 
 app.post("/signup", async (req, res) => {
@@ -72,16 +140,12 @@ app.post("/signup", async (req, res) => {
     }
     throw error
   }
-res.redirect("/");
+  res.redirect("/");
   // res.json({
   //   status: 'ok'
   // })
   // console.log(password);
   // console.log(await bcrypt.hash(password,10));
-});
-
-app.post("/search",function(req,res){
-  res.send("Hello");
 });
 
 app.post("/login", async (req, res) => {
@@ -112,9 +176,7 @@ app.post("/login", async (req, res) => {
     //   status: 'ok'
     // })
     res.render("home");
-  }
-
-  else{
+  } else {
     return res.json({
       status: 'error',
       error: 'Invalid username/password'
